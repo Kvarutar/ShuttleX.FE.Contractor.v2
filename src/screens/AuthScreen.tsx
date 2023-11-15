2e62d120b406c50d4b471ab6d79bbd2268a99bf0
Nikolai <69255437+NikolaB131@users.noreply.github.com>
2023-11-15 13:52:58 +0300

    [Task-95] Contractor - Theme setup (#11)

--- src/screens/AuthScreen.tsx
+++ src/screens/AuthScreen.tsx
@@ -1,59 +1,60 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import { Text } from 'react-native-paper';
-import { ContainedButton, GroupedBrandIcon, TextButton } from 'shuttlex-integration';
+import { Pressable, StyleSheet, View } from 'react-native';
+import { Button, GroupedBrandIcon, sizes, Text, useTheme } from 'shuttlex-integration';
 
-const AuthScreen = (): JSX.Element => (
-  <View style={styles.container}>
-    <View style={styles.groupedBrandIconContainer}>
-      <GroupedBrandIcon />
-      <Text style={styles.titleApp}>Driver App</Text>
-    </View>
+const AuthScreen = (): JSX.Element => {
+  const { colors } = useTheme();
 
-    <View style={styles.buttonsContainer}>
-      <ContainedButton
-        text="Get started" //TODO: Adjust after implement https://www.notion.so/shuttlex/React-Native-Paper-Theme-setup-d194000d1d594e688831b0e5d81cf038
-        style={styles.containedButton} //TODO: Adjust after implement https://www.notion.so/shuttlex/React-Native-Paper-Theme-setup-d194000d1d594e688831b0e5d81cf038
-      />
-      <TextButton
-        text="I already have an account" //TODO: Adjust after implement https://www.notion.so/shuttlex/React-Native-Paper-Theme-setup-d194000d1d594e688831b0e5d81cf038
-        style={styles.textButton} //TODO: Adjust after implement https://www.notion.so/shuttlex/React-Native-Paper-Theme-setup-d194000d1d594e688831b0e5d81cf038
-      />
+  const computedStyles = StyleSheet.create({
+    container: {
+      backgroundColor: colors.backgroundPrimaryColor,
+    },
+  });
+
+  return (
+    <View style={[styles.container, computedStyles.container]}>
+      <View style={styles.groupedBrandIconContainer}>
+        <GroupedBrandIcon />
+        <Text style={styles.titleApp}>Driver App</Text>
+      </View>
+
+      <View style={styles.buttonsContainer}>
+        <Button
+          text="Get started" // TODO: Adjust after implementing https://www.notion.so/shuttlex/i18n-install-library-to-support-i18n-77e236ccfc344d67b9d370e400d45557
+        />
+        <Pressable style={styles.alreadyHaveAccountContainer} onPress={() => {}} hitSlop={20}>
+          {/* // TODO: Adjust after implementing https://www.notion.so/shuttlex/i18n-install-library-to-support-i18n-77e236ccfc344d67b9d370e400d45557 */}
+          <Text style={styles.alreadyHaveAccountText}>I already have an account</Text>
+        </Pressable>
+      </View>
     </View>
-  </View>
-);
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    paddingHorizontal: 24,
-    paddingTop: 32,
-    paddingBottom: 34,
-  },
-  containedButton: {
-    width: 374,
-    height: 48,
-  },
-  textButton: {
-    width: 374,
-    height: 48,
-  },
-  buttonsContainer: {
-    gap: 10,
+    paddingHorizontal: sizes.paddingHorizontal,
+    paddingVertical: sizes.paddingVertical,
   },
   groupedBrandIconContainer: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    gap: 22,
+    gap: 32,
   },
   titleApp: {
     textTransform: 'uppercase',
-    fontSize: 16,
-    color: '#000000',
-    fontWeight: '600',
-    letterSpacing: 0.64,
+    fontFamily: 'Inter SemiBold',
+  },
+  buttonsContainer: {
+    gap: 28,
+  },
+  alreadyHaveAccountContainer: {
+    alignSelf: 'center',
+  },
+  alreadyHaveAccountText: {
+    fontFamily: 'Inter Medium',
   },
 });
 
