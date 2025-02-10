281af109d16256de9a1f1b8652b9efb9ea81bc7d
Karim Zerman <karim.zerman@shuttlex.com>
2025-02-10 12:28:17 +0200

    [Task-528] Integration - Connect redux-persist (#309)
    * [Task-528] Integration - Connect redux-persist
    
    * Remove excess import

--- src/core/redux/store.ts
+++ src/core/redux/store.ts
@@ -1,10 +1,12 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import * as Sentry from '@sentry/react-native';
+import { FLUSH, PAUSE, PERSIST, persistReducer, persistStore, PURGE, REGISTER, REHYDRATE } from 'redux-persist';
 
 import reactotron from '../../../ReactotronConfig';
 import authReducer from '../auth/redux';
 import docsReducer from '../auth/redux/docs';
 import contractorReducer from '../contractor/redux';
+import { MMKVStorage } from '../localStorage';
 import accountSettingsReducer from '../menu/redux/accountSettings';
 import notificationsReducer from '../menu/redux/notifications';
 import subscriptionReducer from '../menu/redux/subscription';
@@ -15,6 +17,14 @@ import mapReducer from '../ride/redux/map';
 import tripReducer from '../ride/redux/trip';
 import signalRReducer from './signalr';
 
+//JUST EXAMPLE
+//TODO: Add some configs like this when work with data persisting
+const tripPersistConfig = {
+  key: 'trip',
+  storage: MMKVStorage,
+  whitelist: [],
+};
+
 const rootReducer = combineReducers({
   auth: authReducer,
   docs: docsReducer,
@@ -23,7 +33,9 @@ const rootReducer = combineReducers({
   alerts: alertsReducer,
   geolocation: geolocationReducer,
   map: mapReducer,
-  trip: tripReducer,
+  //JUST EXAMPLE
+  //TODO: Change to other reducer when work with data persisting
+  trip: persistReducer<ReturnType<typeof tripReducer>>(tripPersistConfig, tripReducer),
   contractor: contractorReducer,
   signalr: signalRReducer,
   accountSettings: accountSettingsReducer,
@@ -37,7 +49,15 @@ const devEnhancers = __DEV__ ? [reactotron.createEnhancer!()] : [];
 export const store = configureStore({
   reducer: rootReducer,
   enhancers: [...devEnhancers, sentryReduxEnhancer],
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
 
 export type AppState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
